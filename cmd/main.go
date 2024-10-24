7a59b7c7e54c1cba053465d41cb5f6eb6172eabe
Tariq Zaitoun <tariq.s.zaitoun@gmail.com>
2024-10-24 19:50:07 -0400

    Added Create Account and Get Account endpoints

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"BankSystem/internal/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var accounts []model.Account
+
+func main() {
+	// Initializing the router
+	router := gin.Default()
+
+	router.POST("/accounts", createAccount)
+	router.GET("/accounts", getAccounts)
+
+	router.Run(":9090")
+}
+
+// the *gin.Context is a reference to the actual Gin Context not a copy.
+func createAccount(context *gin.Context) {
+	var newAccount model.Account
+
+	// ShouldBindJSON takes the data from the client's request and checks if
+	// it is in the correct JSON format, if it is then it will return nil.
+	// we use &(address of) because the ShouldBindJSON fills the newData with the
+	// Client's request
+	if err := context.ShouldBindJSON(&newAccount); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	newAccount.ID = int64(len(accounts) + 1)
+	accounts = append(accounts, newAccount)
+
+	context.JSON(http.StatusCreated, newAccount)
+}
+
+func getAccounts(context *gin.Context) {
+	context.JSON(http.StatusOK, accounts)
+}
